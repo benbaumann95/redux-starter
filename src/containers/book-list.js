61452e674984e69c353c012de70528eefaf02435
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-05 21:00:24 +0100

    function to create and call

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,7 +7,10 @@ class BookList extends React.Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className='list-group-item'>
           {book.title}
         </li>
       );
@@ -43,4 +46,7 @@ function mapDispatchToProps(dispatch) {
 //Whenever the application state changes,
 //the object in the state funciton will be assigned as props to the function
 
+//Promote BookList from a component to a container -
+//it needs to know about this new dispatch method, selectBook.
+//Make it available as a prop
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
